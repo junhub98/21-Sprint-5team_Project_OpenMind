1ea40ecbdd9bf70e23d40ead9da6e89579c09a82
junhub98 <helpwoodjs@icloud.com>
2025-12-29 10:28:51 +0900

    fix: mainPage button Ui

--- src/components/MainPage/MainHeader/MainHeader.styles.js
+++ src/components/MainPage/MainHeader/MainHeader.styles.js
@@ -8,7 +8,7 @@ export const Header = styled.header`
   right: 48px;
   z-index: 10;
   display: flex;
-  justify-content: flex-end;
+  justify-content: center;
   padding: 0;
 
   ${media.tablet`
@@ -23,27 +23,43 @@ export const Header = styled.header`
     right: auto;
     z-index: auto;
     width: 320px;
+    height: 42px;
     justify-content: center;
   `}
 `;
 
 export const GoQuestionsButton = styled.button`
-  padding: 12px 24px;
+  width: 161px;
+  height: 46px;
+  position: relative;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0 20px;
   border-radius: 8px;
-  border: 1px solid #b88a5a;
-  background-color: #fff7f0;
-  color: var( --brown-40);
+  border: 1px solid var(--brown-40);
+  background-color: var(--brown-10);
+  color: var(--brown-40);
   font-size: 16px;
   font-weight: 600;
   cursor: pointer;
 
-  ${media.tablet`
-    padding: 10px 16px;
+  ${media.mobile`
+  top: 0px;
+   width: 123px;
+  height: 34px;
+    padding: 8px 12px;
     font-size: 14px;
   `}
+`;
+
+export const Arrow = styled.img`
+  position: absolute;
+  top: 14px;
+  right: 23px;
 
   ${media.mobile`
-    padding: 8px 12px;
-    font-size: 13px;
+    top: 7.5px;
+    right: 9px;
   `}
 `;
