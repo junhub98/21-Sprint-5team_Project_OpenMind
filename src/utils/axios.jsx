9749fe84b8d28fc18549d6a61430927b68ec9b32
junhub98 <helpwoodjs@icloud.com>
2025-12-30 18:20:22 +0900

    fix: axios baseURL (team)

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,14 +1,8 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-
   // 필요하면 .env에 VITE_OPENMIND_API_BASE_URL로 덮어쓸 수 있습니다.
-  baseURL:
-    import.meta.env.VITE_OPENMIND_API_BASE_URL ??
-    'https://openmind-api.vercel.app/19-1',
-
-
-
+  baseURL: import.meta.env.VITE_OPENMIND_API_BASE_URL ?? 'https://openmind-api.vercel.app/21-5',
 });
 
 export default axios;
